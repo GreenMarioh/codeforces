052e7934cc0ed58a58e1492e6eb87efd7960a5bf
GreenMarioh <mohnishkumar360@gmail.com>
2026-01-18 07:16:52 +0530

    2192

---
+++ Extras/2192A.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+#define co cout <<
+#define ci cin >>
+
+#define vi vector<int>
+#define vll vector<long long>
+
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+#define meow                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9 + 5;
+const ll LINF = 1e18;
+
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define per(i, a, b) for (int i = b - 1; i >= a; --i)
+#define all(x) x.begin(), x.end()
+
+void solve()
+{
+    int n;
+    ci n;
+    vector<int> a(n);
+    rep(i, 0, n) ci a[i];
+
+    for(int i = 0; i < n-1; i++){
+        if ((a[i]&1) == (a[i+1]&1)){
+            co "NO\n";
+            return;
+        }
+    }
+    co "YES\n";
+}
+
+int main()
+{
+    meow;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
