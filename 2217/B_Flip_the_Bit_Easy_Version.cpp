b24ee2efd4b24a670ed80473e34a6d8155d4721d
GreenMarioh <mohnishkumar360@gmail.com>
2026-04-13 00:33:24 +0530

    2217

---
+++ 2217/B_Flip_the_Bit_Easy_Version.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+#define co cout <<
+#define ci cin >>
+
+#define ded << "\n";
+
+#define vi vector<int>
+#define vll vector<long long>
+
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+#define meow                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9 + 5;
+const ll LINF = 1e18;
+
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define per(i, a, b) for (int i = b - 1; i >= a; --i)
+#define all(x) x.begin(), x.end()
+
+#define getv(a,n) \
+    vector<int> a(n); \
+    for(int i=0;i<n;i++) cin >> a[i];
+
+void solve()
+{
+    int n, k; ci n >> k;
+    vi a(n+2);
+    for(int i = 1; i <= n; i++) ci a[i];
+    cin >> k;
+    a[0] = a[n+1] = a[k];
+    int l = 0, r =0;
+    rep(i, 0, k){
+        if (a[i] != a[i+1]) l++;
+    }
+    rep(i, k, n+1){
+        if (a[i] != a[i+1]) r++;
+    }
+    co max(l, r) ded;
+
+    
+}
+
+int main()
+{
+    meow;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
