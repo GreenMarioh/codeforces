2426f9a54483930d81caf16e81ace4eb45164fc9
GreenMarioh <mohnishkumar360@gmail.com>
2026-05-16 19:48:23 +0530

    Extras

--- 2224/C_Zhily_and_Bracket_Swapping.cpp
+++ 2224/C_Zhily_and_Bracket_Swapping.cpp
@@ -34,10 +34,44 @@ const ll LINF = 1e18;
     vector<int> a(n); \
     for(int i=0;i<n;i++) cin >> a[i];
 
+
+
 void solve()
 {
     int n; ci n;
-    getv(a, n);
+    string a, b;
+    ci a;
+    ci b;
+
+    int totLeft = 0;
+    for(char i:a) if (i == '(') totLeft++;
+    for(char i:b) if (i == '(') totLeft++;
+
+    if (totLeft != n){
+        co "NO\n"; return;
+    }
+
+    int c1 = 0, c2 = 0;
+
+    for(int i = 0; i <n; i++){
+        if (a[i] == '(' && b[i] == '(') {
+            c1++; c2++;
+        }
+        else if (a[i] == ')' && b[i] == ')'){
+            c1--; c2--;
+        }
+        else {
+            if (c1 <c2) {c1++; c2--;}
+            else {c1--; c2++;}
+        }
+
+        if (c1 < 0 || c2 < 0) {
+            co "NO\n"; return;
+        }
+    }
+    co "YES\n";
+    
+
     
 }
 
