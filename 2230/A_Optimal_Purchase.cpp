8e655b8a7f3c509d6537596d204b5feb43242e8f
GreenMarioh <mohnishkumar360@gmail.com>
2026-05-18 22:13:39 +0530

    2230

---
+++ 2230/A_Optimal_Purchase.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+#define co cout <<
+#define ci cin >>
+
+#define ded << "\n";
+
+#define vi vector<int>
+#define vll vector<long long>
+
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+#define meow                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9 + 5;
+const ll LINF = 1e18;
+
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define per(i, a, b) for (int i = b - 1; i >= a; --i)
+#define all(x) x.begin(), x.end()
+
+#define getv(a,n) \
+    vector<int> a(n); \
+    for(int i=0;i<n;i++) cin >> a[i];
+
+void solve()
+{
+
+    ll n, a, b;
+    ci n >> a >> b;
+    
+    ll onePrice = min(a, b);
+    ll twoPrice = min(2 * a, b);
+    ll threePrice = min(3 * a, b);
+
+    ll res = (n / 3) * threePrice;
+
+    int rem = n % 3;
+
+    if (rem == 1)
+        res += onePrice;
+    else if (rem == 2)
+        res += twoPrice;
+
+    co res ded
+    
+    
+}
+
+int main()
+{
+    meow;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
